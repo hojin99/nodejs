711ea2a636c6052e3f8e655995a43ce7307cde81
조호진 <hojin99@naver.com>
2022-09-03 11:58:40 +0900

    path.resolve ->path.join

--- src/static_web/server.js
+++ src/static_web/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // static 웹 서버 
 console.log('create server');
-var file = new node_static.Server(path.resolve(__dirname, "www"));
+var file = new node_static.Server(path.join(__dirname, "www"));
 http.createServer(function (req, res) {
     console.log(req.url);
     file.serve(req, res);
