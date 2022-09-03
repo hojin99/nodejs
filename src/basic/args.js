05b49f9dba25f25cb84cf6660497882c358a2034
조호진 <hojin99@naver.com>
2022-09-03 11:13:54 +0900

    log 내용 수정

--- src/basic/args.js
+++ src/basic/args.js
@@ -2,10 +2,10 @@
 // .env를 통한 환경값
 require('dotenv').config();
 
-console.log(process.env.udr_def_path);
-console.log(process.env.udr_exp_dir);
-console.log(process.env.udr_exp_type);
-console.log(process.env.udr_index_url);
+console.log('process.env.udr_def_path : ' + process.env.udr_def_path);
+console.log('process.env.udr_exp_dir : ' + process.env.udr_exp_dir);
+console.log('process.env.udr_exp_type : ' + process.env.udr_exp_type);
+console.log('process.env.udr_index_url : ' + process.env.udr_index_url);
 
 // minimist - 인자값 자동 파싱
 const args = require('minimist')(process.argv.slice(2));
@@ -15,7 +15,7 @@ console.log(args['udr_exp_dir']);
 // 인자값
 process.argv.forEach((element,index) => {
     console.log(`${index}:${element}`);
-    console.log("%d:%s", index, element);
+    // console.log("%d:%s", index, element);
 });
 
 
