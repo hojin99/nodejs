82aebed50a4fefe499af37654371b7686116509a
조호진 <hojin99@naver.com>
2022-11-02 22:49:39 +0900

    프로세스 간 stdin,stdout 통신

--- src/process/main.js
+++ src/process/main.js
@@ -1,6 +1,7 @@
 const child_process = require('child_process');
 const exeName = 'node';
 
+// child process 생성
 const child = child_process.spawn(exeName, ['src/process/sub.js']);
 
 child.on('error', err => {
@@ -15,9 +16,14 @@ child.on('exit', (code, signal) => {
     console.log(`pid ${child.pid} exited with code ${code}, signal ${signal}`);
 });
 
+// child process로 부터 message를 받으면, 1초 후 다시 message 전달
 child.stdout.on('data', data => {
     let dataStr = data.toString();
     console.log(`pid ${child.pid} : ${dataStr}`);
+
+    setTimeout(() => {
+        child.stdin.write('test\r\n');
+    },1000);
 });
 
 child.stderr.on('data', data => {
@@ -25,10 +31,12 @@ child.stderr.on('data', data => {
     console.log(`pid ${child.pid} : ${dataStr}`);
 });
 
+// child process에 초기 message 전달
+child.stdin.write('test\r\n');
+
+// 10초 후 child process에 종료 메시지
 setTimeout(() => {
-    child.stdin.write('test\r\n');
     child.stdin.write('exit\r\n');
-    child.stdin.end();
 },10000);
 
 
--- src/process/sub.js
+++ src/process/sub.js
@@ -2,6 +2,8 @@ let intervalId;
 
 let stdin = process.openStdin();
 
+// stdin으로 main process로 부터 데이터를 받으면 표준 출력으로 echo 메세지 출력
+// exit 메세지를 받으면 종료
 stdin.on('data', (chunk) => {
     let input = chunk.toString();
     input = input.substring(0,input.length-2);
@@ -14,6 +16,4 @@ stdin.on('data', (chunk) => {
     }
 });
 
-setInterval(() => {
-    console.log('.');
-},1000);
+
